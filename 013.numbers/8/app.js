e229d7ee54cac2d2b7f3d866fc93e13339cdbb8c
handyDominant <acsic666@mail.ru>
2023-06-28 21:31:53 +0300

    First added page

---
+++ 013.numbers/8/app.js
@@ -0,0 +1,8 @@
+// 8. Найдите квадратный корень из 245. Предварительно изучите модуль Math и в
+// отдельности Math.sqrt
+
+let Num = 25;
+let SquareRoot = Math.sqrt(25);
+console.log(SquareRoot);  // Вывод: 5
+
+
