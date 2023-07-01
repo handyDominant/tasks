71f10c96306733ec5d16a471f50660cf742968f1
handyDominant <acsic666@mail.ru>
2023-07-01 16:54:31 +0300

    First added page

--- 014.numbers/1/app.js
+++ 014.numbers/1/app.js
@@ -1,6 +1,17 @@
-// 1. Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b.
+// 1. Найти максимальное число из 2 введенных (2 способа)
 
-let a = 10;
-let b = 3;
 
-console.log(a % b);
+let a = prompt('Введите первое значение: ');
+let b = prompt('Введите второе значение: ');
+
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log(b);
+} else {
+    console.log("Числа равны.");
+}
+
+let maxNumber = Math.max(a, b);
+
+console.log("Максимальное число: " + maxNumber);
