0e0ab3a162d4e5fa24582584a2664fbaad73eb81
handyDominant <acsic666@mail.ru>
2023-07-06 23:32:19 +0300

    First added page

--- String/3/app.js
+++ String/3/app.js
@@ -2,10 +2,10 @@
 // то узнать какая у нее длина. Привести к одному регистру, убрать лишние пробелы
 
 // let inputUser = 'Введите, пожалуйста, значение: ';
-let inputUser = 9;
+ let inputUser = 9;
 // let inputUser = 9.0;
 
-console.log(inputUser === isNaN ? console.log(inputUser.toLowerCase().trim()): console.log('ошибка ввода'));
+console.log(inputUser === isNaN ? console.log('ошибка ввода'): console.log(inputUser.toLowerCase().trim()));
    
 
 
