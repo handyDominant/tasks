df450699d6d05c83b0160477e71e887024311dd3
handyDominant <acsic666@mail.ru>
2023-07-06 23:24:04 +0300

    First added page

--- String/3/app.js
+++ String/3/app.js
@@ -1,24 +1,11 @@
 // 3. Ввести строку. Проверить на isNaN. Если число, то ошибка ввода. Если это строка
 // то узнать какая у нее длина. Привести к одному регистру, убрать лишние пробелы
 
-let inputUser = 'Введите, пожалуйста, значение: ';
-// let inputUser = 9;
+// let inputUser = 'Введите, пожалуйста, значение: ';
+let inputUser = 9;
 // let inputUser = 9.0;
 
-if (isNaN) {
-    switch (typeof(String() === inputUser)) {
-        case inputUser:
-            console.log('Не isNan')
-            console.log('то ошибка ввода')
-            
-
-        default:
-            console.log(inputUser.toLowerCase().trim())
-            break;
-    }
-} else {
-    console.log('три кита')
- } 
+console.log(inputUser === isNaN ? console.log(inputUser.toLowerCase().trim()): console.log('ошибка ввода'));
    
 
 
