5c116cd2703e7e62ccbb363a2ed459f51fef1810
handyDominant <acsic666@mail.ru>
2023-07-10 22:37:39 +0300

    First added page

--- String/7/app.js
+++ String/7/app.js
@@ -1,7 +1,7 @@
 // 7. На вход программе подаётся строка. Замените все @ на '!' (2 способа)
 
 let str = 'test@'
-let newStr = str.replace(/@/g, '!');
+let newStr = str.replace(/@/, '!');
 
 console.log(newStr);
 
