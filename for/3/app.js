80267cc93e6cd6df33534c21740a5d9cf9628b46
handyDominant <acsic666@mail.ru>
2023-07-18 21:30:45 +0300

    First added page

--- for/3/app.js
+++ for/3/app.js
@@ -1,20 +1,20 @@
 // 3. На вход подается число. Необходимо вывести все числа от 1 до введенного числа
 // кратные 3.
 
-let inputUser = 'Введите текст или числа';
+let inputUser = 3030202030;
+var logInformation = [];
 
+// Преобразуем inputUser в строку, чтобы можно было обращаться к его цифрам
+inputUser = inputUser.toString();
 
-if (inputUser === Number.inputUser){
-        for (i = 0; i < inputUser.length(); i++){
-            if (inputUser == 3){
-                console.log(inputUser)
-        }
+for (let i = 0; i < inputUser.length; i++) {
+    if (inputUser[i] === '3') {
+        logInformation.push(inputUser[i]);
     }
-    
 }
 
 
-
+console.log(logInformation.join());
 
 
 
