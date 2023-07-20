186f4d311ee696e027f17b338546cf3f823a216f
handyDominant <acsic666@mail.ru>
2023-07-20 23:33:12 +0300

    First added page

--- for/5/app.js
+++ for/5/app.js
@@ -1,16 +1,8 @@
-// 5. Ввести строку. Проверить является ли это число или строка. Если число, то
-// ошибка ввода. Если это строка то первый символ поставить в верхний регистр.
-// Убрать лишние пробелы
+// 5. Найдите максимальное значение в статичном массиве используя цикл.
 
-let inputUser = 'введите, пожалуйста, значение: ';
-// let inputUser = Number(9);
-// let inputUser = parseFloat(9.0);
+let inputUserNamber = 'Введите, пожалуйста, число!';
 
-if (inputUser === !String(inputUser)){
-    console.log('Ошибка ввода');
-} else {
-    console.log(inputUser[0].toUpperCase() + inputUser.slice(1).trim());
-}
+console.log(inputUserNamber);
 
 
 
