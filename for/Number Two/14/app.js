a096ea63490afc88fb5bcb1f5012e46653eccef0
handyDominant <acsic666@mail.ru>
2023-07-31 17:41:33 +0300

    First added page

--- for/Number Two/14/app.js
+++ for/Number Two/14/app.js
@@ -1,10 +1,18 @@
 // 14. Дан массив числами, например: [10, 20, 30, 50, 235, 3000]. Выведите на экран только
 // те числа из массива, которые начинаются на цифру 1, 2 или 5 -> 10, 20, 50, 235
 
-let inputUser = [10, 20, 30, 50, 235, 3000];
+let inputUser  = [10, 20, 30, 50, 235, 3000].toString().split(',');
+let helpFileInputUser = [];
+let itemsInputUser = [];
 
-  for(i < 0; i < inputUser.length; i++){
-     console.log(inputUser)
-  }
+for(helpFileInputUser of inputUser){
+   if(helpFileInputUser[0] === '1' || 
+      helpFileInputUser[0] === '2' || 
+      helpFileInputUser[0] === '5'){
+
+         itemsInputUser.push(helpFileInputUser);
+   }
+} console.log(itemsInputUser)
+
 
 
