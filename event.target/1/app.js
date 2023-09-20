aad0f7709e5797bedd4d263fe2415b3044aca225
handyDominant <acsic666@mail.ru>
2023-09-20 18:33:26 +0300

    First added page

--- event.target/1/app.js
+++ event.target/1/app.js
@@ -1,13 +1,14 @@
-// 1. Необходимо отобразить кнопку с надписью «Нажми на меня». По клику на нее
-// вывести alert с сообщением
+// На странице расположен маркированный список. Пользователь кликает на
+// каждый item списка. Необъодимо отловить на какой из элементов нажал
+// пользователь и отобразить
 
+const ulTag = document.querySelector('ul');
 
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', function(){
-    alert('Hi')
-});
-
+ulTag.addEventListener('click', function(event){
+    const divEl = document.querySelector('div');
+    divEl.innerHTML = event.target.textContent;
+    
+})
 
 
 
--- event.target/2/app.js
+++ event.target/2/app.js
@@ -1,12 +1,14 @@
-// 2. Необходимо отобразить кнопку с надписью «Нажми на меня» и инпут со
-// значением по-умолчанию «+375(хх)ххх-хх-хх». По клику на кнопку заменить
-// значение инпута на «+375(29)111-11-11»
-
+// По наведению на кнопку вывести сообщение «Hi». Когда мышка выходит за
+// границы кнопки сновка скрывать
 
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(){
-    const inp = document.querySelector('input');
-    inp.value = '+375(29)111-11-11';
+btn.addEventListener('mouseover', () => {
+    const div = document.querySelector('div');
+    div.innerHTML = 'Hi';
+})
 
+btn.addEventListener('mouseout', () => {
+    const div = document.querySelector('div');
+    div.innerHTML = ' ';
 })
