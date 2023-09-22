cbc6b8364a841b5ccaf38d9905a5f6c489b36928
handyDominant <acsic666@mail.ru>
2023-09-22 19:34:41 +0300

    First added page

--- event.target/6/app.js
+++ event.target/6/app.js
@@ -1,18 +1,22 @@
 // Записать в результат удвоенное значение инпута. Добавить проверки 
 
-let btn = document.getElementById('dobleValueButton');
-let inp = document.getElementById('myInput');
+const doubleValueButton = document.getElementById('doubleValueButton');
+const myInput = document.getElementById('myInput');
 
-try{
-    if(!isNaN) throw new Error('Введите число!');
-    btn.addEventListener('click', function(){
-        let result = inp.value * 2;
-        console.log(result);
-    })
 
-}catch(error){
-    console.log(error.Message)
+try {
+    if (isNaN(myInput.value)) {
+        throw new Error('Пожалуйста, введите число!');
+    }
 
+    doubleValueButton.addEventListener('click', function() {
+        const inputValue = parseInt(myInput.value);
+        const doubledValue = inputValue * 2;
+        console.log(doubledValue);
+    });
+
+} catch (error) {
+    console.error(error.message);
 }
 
 
