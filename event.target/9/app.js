d2551c7706fb6218f1c9c10e80d6274f998873fe
handyDominant <acsic666@mail.ru>
2023-09-24 20:34:02 +0300

    First added page

--- event.target/9/app.js
+++ event.target/9/app.js
@@ -1,15 +1,1 @@
 // У вас есть кнопка. После каждого нажатия менять background.
-
-const button = document.getElementById('changeBackgroundButton');
-const content = document.getElementById('content');
-const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff']; // Массив цветов
-
-let currentColorIndex = 0;
-
-button.addEventListener('click', () => {
-  // Изменяем фоновый цвет на следующий цвет из массива
-  content.style.backgroundColor = colors[currentColorIndex];
-  
-  // Увеличиваем индекс текущего цвета
-  currentColorIndex = (currentColorIndex + 1);
-});
